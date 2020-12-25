d27647c6616bc0dfba049c3348a9be7cb5d5f223
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-25 16:45:37 -0400

    Test cases 4 and 5 passed

--- main_test.go
+++ main_test.go
@@ -6,17 +6,32 @@ import
 
 func TestConvertion1(t *testing.T){
 	if convertionToMorse("hola") != ".... --- .-.. .- " {
-		t.Error("wrong")
+		t.Error("wrong case 1")
 	}
 }
 func TestConvertion2(t *testing.T){
 	if convertionToMorse("test") != "- . ... - " {
-		t.Error("wrong")
+		t.Error("wrong case 2")
 	}
 }
 func TestConvertion3(t *testing.T){
 	if convertionToMorse("cocina") != "-.-. --- -.-. .. -. .- " {
-		t.Error("wrong")
+		t.Error("wrong case 3")
+	}
+}
+func TestConvertion4(t *testing.T){
+	if convertionToMorse("queso") != "--.- ..- . ... --- " {
+		t.Error("wrong case 4")
+	}
+}
+func TestConvertion5(t *testing.T){
+	if convertionToMorse("camaleon") != "-.-. .- -- .- .-.. . --- -. " {
+		t.Error("wrong case 5")
+	}
+}
+func TestConvertion6(t *testing.T){
+	if convertionToMorse("hp") != ".... .--. " {
+		t.Error("wrong case 6")
 	}
 }
 
