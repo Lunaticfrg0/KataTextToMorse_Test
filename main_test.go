4e245016b434207b3ee4495d358089243ec69ec8
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-26 16:09:11 -0400

    Test case 2 passed

--- main_test.go
+++ main_test.go
@@ -9,3 +9,8 @@ func TestConvertion1(t *testing.T){
 		t.Error("wrong case 1")
 	}
 }
+func TestConvertion2(t *testing.T){
+	if convertToMorse("tucan") != "..- -.-. .- -. "  {
+		t.Error("wrong case 2")
+	}
+}
