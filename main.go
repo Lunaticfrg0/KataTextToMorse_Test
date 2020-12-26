7854f04320243fc0fbd3a04d73d52583c124ed00
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-26 16:08:14 -0400

    test case 1 passed

--- main.go
+++ main.go
@@ -1,18 +1,77 @@
 package main
 import (
 	"fmt"
+	"strings"
 	)
+
+var morseDicctionaty = map[string]string{
+	"A": ".-",
+	"B": "-...",
+	"C": "-.-.",
+	"D": "-..",
+	"E": ".",
+	"F": "..-.",
+	"G": "--.",
+	"H": "....",
+	"I": "..",
+	"J": ".---",
+	"K": "-.-",
+	"L": ".-..",
+	"M": "--",
+	"N": "-.",
+	"O": "---",
+	"P": ".--.",
+	"Q": "--.-",
+	"R": ".-.",
+	"S": "...",
+	"T": "-",
+	"U": "..-",
+	"V": "...-",
+	"W": ".--",
+	"X": "-..-",
+	"Y": "-.--",
+	"Z": "--..",
+	"1": ".----",
+	"2": "..---",
+	"3": "...--",
+	"4": "....-",
+	"5": ".....",
+	"6": "-....",
+	"7": "--...",
+	"8": "---..",
+	"9": "----.",
+	"0": "-----",
+	".": ".-.-.-",
+	",": "--..--",
+	"?": "..--..",
+	"!": "-.-.--",
+	"-": "-....-",
+	"/": "-..-.",
+	"@": ".--.-.",
+	"(": "-.--.",
+	")": "-.--.-",
+}
+
+func toMorseCode(word, splitter string) string {
+	var morse string
+	word = strings.ToUpper(word)
+
+	for i:= 0 ; i< len(word); i++{
+		code := morseDicctionaty[word[i:i+1]]
+		if code != ""{
+			morse += code + splitter
+		}
+	}
+	return morse
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
 func convertToMorse(word string) string	{
-	if word == "hola"{
-		return ".... --- .-.. .- "
-	} else{
-		return word
-	}
+	var splitter = " "
+	return toMorseCode(word, splitter)
 }
 
 
