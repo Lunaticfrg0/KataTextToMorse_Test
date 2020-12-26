fe56d310ba8e8a891f1fc83a49023d9c3b7ef3c2
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-26 16:01:09 -0400

    Initial pass of Test case 1

--- main_test.go
+++ main_test.go
@@ -5,37 +5,7 @@ import
 
 
 func TestConvertion1(t *testing.T){
-	if convertionToMorse("hola") != ".... --- .-.. .- " {
+	if convertToMorse("hola") != ".... --- .-.. .- "  {
 		t.Error("wrong case 1")
-	}
-}
-func TestConvertion2(t *testing.T){
-	if convertionToMorse("test") != "- . ... - " {
-		t.Error("wrong case 2")
-	}
-}
-func TestConvertion3(t *testing.T){
-	if convertionToMorse("cocina") != "-.-. --- -.-. .. -. .- " {
-		t.Error("wrong case 3")
-	}
-}
-func TestConvertion4(t *testing.T){
-	if convertionToMorse("queso") != "--.- ..- . ... --- " {
-		t.Error("wrong case 4")
-	}
-}
-func TestConvertion5(t *testing.T){
-	if convertionToMorse("camaleon") != "-.-. .- -- .- .-.. . --- -. " {
-		t.Error("wrong case 5")
-	}
-}
-func TestConvertion6(t *testing.T){
-	if convertionToMorse("hp") != ".... .--. " {
-		t.Error("wrong case 6")
-	}
-}
-func TestConvertion7(t *testing.T){
-	if convertionToMorse("1234567") != ".---- ..--- ...-- ....- ..... -.... --... " {
-		t.Error("wrong case 7")
 	}
 }
